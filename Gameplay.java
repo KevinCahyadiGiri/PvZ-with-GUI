851a8b71b8eb8b7a1ce1e0d1d77c53be2027d0dd
kevincahyadigiri <kevincahyadigiri@gmail.com>
2020-04-18 00:19:39 +0700

    zombie makin lama makin cepet

--- Gameplay.java
+++ Gameplay.java
@@ -488,7 +488,9 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener, Mou
         try {
             counterGenerateZombie++;
             if (counterGenerateZombie % modGenerateZombie == 0) {
-                modGenerateZombie--;
+                if (modGenerateZombie > 2) {
+                    modGenerateZombie--;
+                }
                 Zombie newZombie = (Zombie) zombieSelection.get(take).clone();
                 zombieList.add(newZombie);
                 System.out.println("take" + take);
